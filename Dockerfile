ee68eef8e81c0b754ffb1579e95784210c58e3c2
Michael Ritter <drissg@gmail.com>
2020-05-24 14:06:50 +0200

    feat(gateway): add autologin

--- Dockerfile
+++ Dockerfile
@@ -8,8 +8,8 @@ RUN sed -i '${s/$/'" --headless"'/}' /opt/google/chrome/google-chrome
 # https://github.com/debuerreotype/docker-debian-artifacts/issues/24
 RUN mkdir -p /usr/share/man/man1
 
-# Install JRE
-RUN apt update && apt install -y openjdk-11-jre-headless unzip && rm -rf /var/lib/apt/lists/*
+# Install JRE, unzip, jq
+RUN apt update && apt install -y openjdk-11-jre-headless unzip jq && rm -rf /var/lib/apt/lists/*
 
 USER webdriver
 
@@ -21,8 +21,9 @@ RUN wget https://download2.interactivebrokers.com/portal/clientportal.gw.zip && 
 
 COPY conf.yaml /home/webdriver/root/conf.yaml
 COPY generateCert.sh /home/webdriver/generateCert.sh
+COPY magic.sh /home/webdriver/magic.sh
 
 EXPOSE 5000
 
-ENTRYPOINT ./generateCert.sh && bin/run.sh root/conf.yaml
+ENTRYPOINT ./generateCert.sh && chromedriver --port=4444 --whitelisted-ips & bin/run.sh root/conf.yaml & sleep 7; while true; do ./magic.sh; sleep 60; done
 CMD ""
---
+++ magic.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+if [[ "$GATEWAY_IP" == "" ]]; then
+    GATEWAY_IP="localhost"
+fi
+
+# check if already logged in
+authenticated=$(curl -ksf https://localhost:5000/v1/portal/iserver/auth/status | jq -r '.authenticated')
+if [[ "$authenticated" == "true" ]]; then
+    echo "Already logged in"
+    exit 0
+fi
+echo "Login expired, logging back in"
+
+ready=$(curl -sf http://localhost:4444/status | jq '.value.ready')
+#echo $ready
+
+if [[ "$ready" != "true" ]]; then
+    echo "Error: webdriver not available"
+    exit 1
+fi
+
+sessionId=$(curl -sf http://localhost:4444/session -X POST -H "Content-Type: application/json" -d '{"capabilities":{"alwaysMatch":{"acceptInsecureCerts":true}}}' | jq -r '.value.sessionId') 
+#echo $sessionId
+trap "curl -sf http://localhost:4444/session/$sessionId -X DELETE -H 'Content-Type: application/json' -d '{}' > /dev/null" EXIT
+
+curl -sf http://localhost:4444/session/$sessionId/url -X POST -H "Content-Type: application/json" -d "{\"url\":\"https://$GATEWAY_IP:5000/\"}" > /dev/null
+
+nameElementId=$(curl -sf http://localhost:4444/session/$sessionId/element -X POST -H "Content-Type: application/json" -d '{"using":"css selector","value":"#user_name"}' | jq -r '.value[]')
+
+curl -sf http://localhost:4444/session/$sessionId/element/$nameElementId/value -X POST -H "Content-Type: application/json" -d "{\"text\": \"$USERNAME\"}" > /dev/null
+
+pwElementId=$(curl -sf http://localhost:4444/session/$sessionId/element -X POST -H "Content-Type: application/json" -d '{"using":"css selector","value":"#password"}' | jq -r '.value[]')
+
+curl -sf http://localhost:4444/session/$sessionId/element/$pwElementId/value -X POST -H "Content-Type: application/json" -d "{\"text\": \"${PASSWORD}\n\"}" > /dev/null
+
+sleep 7
+
+# debug
+#curl -sf http://localhost:4444/session/$sessionId/screenshot | jq -r '.value' | base64 -d > image2.png
+
+# check for 2fa
+
+# check if login successful
+authenticated=$(curl -ksf https://localhost:5000/v1/portal/iserver/auth/status | jq -r '.authenticated')
+if [[ "$authenticated" == "true" ]]; then
+    echo "Login succeeded"
+else
+    echo "Login failed"
+fi
