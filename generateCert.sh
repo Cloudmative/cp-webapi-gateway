cd409f084b1117b4e66e4590ec82bd67623e5456
Michael Ritter <drissg@gmail.com>
2020-08-26 17:38:58 +0200

    Revert "feat(cert): use externally provided SSL certificate"
    This reverts commit 42661585e36d5cea19d6010887937194fe9fd75b.

--- generateCert.sh
+++ generateCert.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-echo "$CLDMTV_CERT_PUBLIC" > combined.pem
-echo "$CLDMTV_CERT_PRIVATE" >> combined.pem
-openssl pkcs12 -export -in combined.pem -out cert.p12 -passout pass:mypass
-keytool -importkeystore -srckeystore cert.p12 -srcstoretype pkcs12 -destkeystore cldmtv-trade-gateway.jks -storepass mypass -srcstorepass mypass
+domain=$(uname -n)
+
+if [[ "$CLDMTV_DOMAIN" != "" ]]; then
+    domain="$CLDMTV_DOMAIN"
+fi
+
+# Generate a keystore.
+keytool -genkey -alias cldmtv-trade-gateway -storepass mypass -keystore cldmtv-trade-gateway.jks \
+    -keypass mypass -keyalg RSA -sigalg SHA1withRSA -dname "CN=$domain, OU=, O=Cloudmative, L=BERN, S=BE, C=CH"
+# Generate a certificate and CSR for the keystore. CN should match the hostname of presto-coordinator
+keytool -export -alias cldmtv-trade-gateway -storepass mypass -keystore cldmtv-trade-gateway.jks \
+    -file cldmtv-trade-gateway.cer && \
+    keytool -certreq -alias cldmtv-trade-gateway -storepass mypass -keystore cldmtv-trade-gateway.jks \
+    -file cldmtv-trade-gateway.csr
